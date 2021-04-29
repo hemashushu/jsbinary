849c25d72b520d847580235098b9861a800c3d2a
Hippo Spark (Arch) <hippospark@gmail.com>
2021-04-29 10:57:51 +0800

    Binary.length 改名为 bitWidth。
    同时增加了 getBits，setBits 方法的单元测试。

--- src/binary.js
+++ src/binary.js
@@ -1,70 +1,93 @@
 const Int32Math = require('../wasm/int32math');
 
-const MAX_BIT_LENGTH = 32;
+const MAX_BIT_WIDTH = 32;
 
-// 二进制数每一位的数值
-const bitMask = new Array(MAX_BIT_LENGTH);
+// 二进制数每位的数值
+const bitMask = new Array(MAX_BIT_WIDTH);
 
-for (let idx = 0; idx < MAX_BIT_LENGTH; idx++) {
+for (let idx = 0; idx < MAX_BIT_WIDTH; idx++) {
     bitMask[idx] = Math.pow(2, idx) | 0;
 }
 
 /**
  * 一个简单的二进制数据类型操作对象。最长支持 32 位二进制数。
  *
- * 备注：
- * 为了提高效率，本类的所有方法都没有对参数（包括 value 和 length）的
- * 有效性进行检测，调用者必须保证参数的有效性。
+ * 注1：
+ *
+ * 为提高效率，本类的大部分方法都没有对参数（包括 value 和 length）的
+ * 有效性进行检测，调用者必须自己保证参数的有效性。
+ *
+ * 注2：
+ *
+ * 如需对两个 Binary 对象作 “乘、除、余数” 算术运算或者 “算术右移”，必须
+ * 保证操作数的位宽一致，且均为 32 位。
  */
 class Binary {
 
     /**
      * 构造 Binary 对象
      *
+     * 这里未对 value, bitWidth 作有效性检测，调用者必须自己保证参数的有效性。
+     *
      * @param {*} value Int32 数值
-     * @param {*} length 取值范围为 1-32，当 value 为负数时，length 必须为 32
+     * @param {*} bitWidth 取值范围为 1-32，当 value 为负数时，length 必须为 32
      */
-    constructor(value, length) {
-        // value 将作为无符号数（uint32_t）来处里，当传入的数字为负数时，由于 JavaScript 缺少
-        // 无符号数据类型，所以读取 value 时仍然会获得负数。
+    constructor(value, bitWidth) {
+
+        // Binary 对象对应的整型数值，其值为 Int32。
+        //
+        // value 将作为无符号数（uint32_t）来处里，当传入的数字
+        // 为负数时，将会转换到无符号数的范围，不过由于 JavaScript 缺少
+        // 无符号数据类型，所以直接读取 value 时，仍然会获得负数。
         this.value = value | 0;
 
-        // 当前 Binary 实现并未对 length 作任何检测。
-        // 对于算术运算，必须保证两个操作数的长度均为 32 位。
-        this.length = length;
+        // Binary 对象的位宽，范围从 1~32。
+        this.bitWidth = bitWidth;
     }
 
     /**
      * 从二进制字符串构造 Binary 对象
      *
      * @param {*} str 二进制数字字符串，最长不超过 32 个字符，字符串中不能有
-     *     除了 [01] 之外的字符。示例：
+     *     除了 [01] 之外的字符，不能出现正负符号。
+     *     正确的示例：
      *     '1010', '11100000'
-     * @param {*} length 1-32
+     * @param {*} bitWidth 1-32
      * @returns
      */
-    static fromBinaryString(str, length) {
-        if (length === undefined) {
-            length = str.length;
+    static fromBinaryString(str, bitWidth) {
+        if (bitWidth === undefined) {
+            // 将字符串的长度作为位宽
+            bitWidth = str.length;
         }
-        return new Binary(parseInt(str, 2), length);
+
+        if (str.length > 32 || bitWidth > 32) {
+            throw new RangeError('Bit width out of range.');
+        }
+
+        return new Binary(parseInt(str, 2), bitWidth);
     }
 
     /**
      * 从十六进制字符串构造 Binary 对象
      *
      * @param {*} str 十六进制数字字符串，最长不超过 8 个字符，字符串中不能有
-     *     除了 [0-9a-fA-F-] 之外的字符。示例：
+     *     除了 [0-9a-fA-F] 之外的字符，不能出现正负符号。
+     *     正确示例：
      *     'FF00', 'DEADBEEF'
-     * @param {*} length 1-32
+     * @param {*} bitWidth 1-32
      * @returns
      */
-    static fromHexString(str, length) {
-        if (length === undefined) {
-            length = str.length * 4;
+    static fromHexString(str, bitWidth) {
+        if (bitWidth === undefined) {
+            bitWidth = str.length * 4;
         }
 
-        return new Binary(parseInt(str, 16), length);
+        if (str.startsWith('-') || str.length > 8 || bitWidth > 32) {
+            throw new RangeError('Bit width out of range.');
+        }
+
+        return new Binary(parseInt(str, 16), bitWidth);
     }
 
     /**
@@ -72,15 +95,20 @@ class Binary {
      *
      * @param {*} str 十进制字符串，最大值不超过 Int32.MAX，字符串仅可包含
      *     [0-9-] 字符。
-     * @param {*} length 1-32
+     * @param {*} bitWidth 1-32
      * @returns
      */
-    static fromDecimalString(str, length) {
+    static fromDecimalString(str, bitWidth) {
         let value = parseInt(str, 10);
-        if (length === undefined) {
-            length = value.toString(2).length;
+        if (bitWidth === undefined) {
+            bitWidth = value.toString(2).length;
         }
-        return new Binary(value, length);
+
+        if (bitWidth > 32) {
+            throw new RangeError('Bit width out of range.');
+        }
+
+        return new Binary(value, bitWidth);
     }
 
     /**
@@ -90,82 +118,121 @@ class Binary {
      * @returns
      */
     static clone(binary) {
-        return new Binary(binary.value, binary.length);
+        return new Binary(binary.value, binary.bitWidth);
     }
 
     /**
-     * 判断两个 Binary 对象是否相等（数值和长度均相等）。
+     * 判断两个 Binary 对象是否相等（数值和位宽均相等）。
+     *
      * @param {*} left
      * @param {*} right
      * @returns
      */
     static equals(left, right) {
         return left.value === right.value &&
-            left.length === right.length;
+            left.bitWidth === right.bitWidth;
     }
 
     static add(left, right) {
-        return new Binary(left.value + right.value, left.length);
+        return new Binary(left.value + right.value, left.bitWidth);
     }
 
     static subtract(left, right) {
-        return new Binary(left.value - right.value, left.length);
+        return new Binary(left.value - right.value, left.bitWidth);
     }
 
     static multiplyLow(left, right) {
         let value = Int32Math.multiplyLow(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static multiplyHigh(left, right) {
         let value = Int32Math.multiplyHigh(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static multiplyHighUnsigned(left, right) {
         let value = Int32Math.multiplyHighUnsigned(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static divide(left, right) {
         let value = Int32Math.divide(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static divideUnsigned(left, right) {
         let value = Int32Math.divideUnsigned(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static remainder(left, right) {
         let value = Int32Math.remainder(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static remainderUnsigned(left, right) {
         let value = Int32Math.remainderUnsigned(left.value, right.value);
-        return new Binary(value, left.length);
+        return new Binary(value, left.bitWidth);
     }
 
     static and(left, right) {
         // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Expressions_and_Operators#bitwise_operators
-        return new Binary(left.value & right.value, left.length);
+        return new Binary(left.value & right.value, left.bitWidth);
     }
 
     static or(left, right) {
-        return new Binary(left.value | right.value, left.length);
+        return new Binary(left.value | right.value, left.bitWidth);
     }
 
+    static nand(left, right) {
+        // nand = not(and)
+        return new Binary(~(left.value & right.value), left.bitWidth);
+    }
+
+    static nor(left, right) {
+        // nor = not(or)
+        return new Binary(~(left.value | right.value), left.bitWidth);
+    }
+
+    /**
+     * 异或
+     * @param {*} left
+     * @param {*} right
+     * @returns
+     */
     static xor(left, right) {
-        return new Binary(left.value ^ right.value, left.length);
+        // In1	In2	Out
+        // 0	0	0
+        // 0	1	1
+        // 1	0	1
+        // 1	1	0
+        return new Binary(left.value ^ right.value, left.bitWidth);
+    }
+
+    /**
+     * 同或
+     * @param {*} left
+     * @param {*} right
+     * @returns
+     */
+    static xnor(left, right) {
+        // xnor = not(xor)
+        //
+        // In1	In2	Out
+        // 0	0	1
+        // 0	1	0
+        // 1	0	0
+        // 1	1	1
+        return new Binary(~(left.value ^ right.value), left.bitWidth);
     }
 
     static not(binary) {
-        return new Binary(~binary.value, binary.length);
+        return new Binary(~binary.value, binary.bitWidth);
     }
 
-    static leftShift(binary, length) {
-        return new Binary(binary.value << length, binary.length);
+    static leftShift(binary, bitWidth) {
+        return new Binary(binary.value << bitWidth, binary.bitWidth);
     }
 
     /**
@@ -174,13 +241,13 @@ class Binary {
      * 当最高位为 0 时，最高位会使用 0 来填充，效果如同逻辑右移。
      *
      * @param {*} binary
-     * @param {*} length 0-32
+     * @param {*} bitWidth 0-32
      * @returns
      */
-    static rightShift(binary, length) {
+    static rightShift(binary, bitWidth) {
         // 001100...00 >> 2 = 000011...00
         // 110000...00 >> 2 = 111100...00
-        return new Binary(binary.value >> length, binary.length);
+        return new Binary(binary.value >> bitWidth, binary.bitWidth);
     }
 
     /**
@@ -188,13 +255,13 @@ class Binary {
      * 最高位将会使用 0 来填充。
      *
      * @param {*} binary
-     * @param {*} length 0-32
+     * @param {*} bitWidth 0-32
      * @returns
      */
-    static logicRightShift(binary, length) {
+    static logicRightShift(binary, bitWidth) {
         // 001100...00 >> 2 = 000011...00
         // 110000...00 >> 2 = 001100...00
-        return new Binary(binary.value >>> length, binary.length);
+        return new Binary(binary.value >>> bitWidth, binary.bitWidth);
     }
 
     /**
@@ -217,12 +284,22 @@ class Binary {
      *
      * @param {*} binary
      * @param {*} offset 目标数值（即当前 Binary 对象数值的）偏移值，注意
-     *     offset + binary.length 必须小于等于 this.length.
+     *     offset + binary.bitWidth 必须小于等于 this.bitWidth.
      *     offset 是从最低位开始数的，比如 0b11110000，最右边（最低位）的 0 的
      *     offset 为 0，最左边（最高位）的 offset 为 7。
      */
     setBits(binary, offset) {
-        for (let idx = 0; idx < binary.length; idx++) {
+        // 示例，如果对如下二进制数调用 setBits('10110', 1)
+        // 10000000
+        // --^---^-
+        //   |   |
+        // 结束  开始
+        //   10110
+        //   |   |
+        //   v   v
+        // 10101100
+
+        for (let idx = 0; idx < binary.bitWidth; idx++) {
             this.setBit(idx + offset, binary.getBit(idx));
         }
     }
@@ -242,14 +319,22 @@ class Binary {
      * 读取（连续的）多位的数值
      *
      * @param {*} offset 当前数值的偏移值，注意
-     *     offset + length 必须小于等于 this.length.
+     *     offset + bitWidth 必须小于等于 this.bitWidth.
      *     offset 是从最低位开始数的，比如 0b11110000，最右边（最低位）的 0 的
      *     offset 为 0，最左边（最高位）的 offset 为 7。
-     * @param {*} length 需读取的长度（位）
+     * @param {*} bitWidth 需读取的位宽（位）
      */
-    getBits(offset, length) {
-        let binary = new Binary(0, length);
-        for (let idx = 0; idx < length; idx++) {
+    getBits(offset, bitWidth) {
+        // 示例，如果对如下二进制数调用 getBits(1, 5)
+        // 10101100
+        // --^---^-
+        //   |   |
+        // 结束  开始
+        //
+        // 将得到 10110
+
+        let binary = new Binary(0, bitWidth);
+        for (let idx = 0; idx < bitWidth; idx++) {
             binary.setBit(idx, this.getBit(idx + offset));
         }
         return binary;
@@ -287,24 +372,36 @@ class Binary {
         return Binary.equals(this, other);
     }
 
+    /**
+     * 转换为 2 进制字符串
+     *
+     * 前导的 0 会被省略
+     * @returns
+     */
     toBinaryString() {
         if (this.value >= 0) {
             return this.value.toString(2);
         } else {
             let buffer = [];
-            for (let idx = this.length - 1; idx >= 0; idx--) {
+            for (let idx = this.bitWidth - 1; idx >= 0; idx--) {
                 buffer.push(this.getBit(idx));
             }
             return buffer.join('');
         }
     }
 
+    /**
+     * 转换为 16 进制字符串
+     *
+     * 前导的 0 会被省略
+     * @returns
+     */
     toHexString() {
         if (this.value >= 0) {
             return this.value.toString(16);
         } else {
             let mask4bits = 0b1111;
-            let remainBits = this.length;
+            let remainBits = this.bitWidth;
 
             let buffer = [];
             while (remainBits >= 4) {
@@ -322,9 +419,14 @@ class Binary {
 
             return buffer.reverse().join('');
         }
-
     }
 
+    /**
+     * 转换为 10 进制字符串
+     *
+     * 前导的 0 会被省略
+     * @returns
+     */
     toDecimalString() {
         return this.value.toString(10);
     }
--- test/test.js
+++ test/test.js
@@ -7,40 +7,40 @@ describe('Binary', () => {
         it('fromBinaryString', () => {
             let b1 = Binary.fromBinaryString('1001', 4);
             assert.equal(b1.value, 0b1001);
-            assert.equal(b1.length, 4);
+            assert.equal(b1.bitWidth, 4);
 
-            // 测试省略 length 参数
+            // 测试省略 bitWidth 参数
             let b2 = Binary.fromBinaryString('10');
             assert.equal(b2.value, 0b10)
-            assert.equal(b2.length, 2)
+            assert.equal(b2.bitWidth, 2)
         });
 
         it('fromHexString', () => {
             let b1 = Binary.fromHexString('FF', 8);
             assert.equal(b1.value, 0xff);
-            assert.equal(b1.length, 8);
+            assert.equal(b1.bitWidth, 8);
 
-            // 测试省略 length 参数
+            // 测试省略 bitWidth 参数
             let b2 = Binary.fromHexString('DEEF');
             assert.equal(b2.value, 0xdeef);
-            assert.equal(b2.length, 16);
+            assert.equal(b2.bitWidth, 16);
         });
 
         it('fromDecimalString', () => {
             let b1 = Binary.fromDecimalString('123', 8);
             assert.equal(b1.value, 123);
-            assert.equal(b1.length, 8);
+            assert.equal(b1.bitWidth, 8);
 
-            // 测试省略 length 参数
+            // 测试省略 bitWidth 参数
             let b2 = Binary.fromDecimalString('1024');
             assert.equal(b2.value, 1024);
-            assert.equal(b2.length, 11);
+            assert.equal(b2.bitWidth, 11);
         });
 
         it('directly', () => {
             let b1 = new Binary(0b1100_0011, 8);
             assert.equal(b1.value, 0xc3);
-            assert.equal(b1.length, 8);
+            assert.equal(b1.bitWidth, 8);
         });
     });
 
@@ -72,6 +72,17 @@ describe('Binary', () => {
             assert.equal(b2.value, 0x7ffffffe);
         });
 
+        it('getBits, setBits', ()=> {
+            let b1 = Binary.fromBinaryString('10101100', 8);
+
+            let bp = b1.getBits(1, 5);
+            assert.equal(bp.toBinaryString(), '10110');
+
+            let b2 = Binary.fromBinaryString('10000000', 8);
+
+            b2.setBits(bp, 1);
+            assert.equal(b2.toBinaryString(), '10101100');
+        });
 
     });
 
@@ -83,6 +94,10 @@ describe('Binary', () => {
             assert.equal(b1.toDecimalString(), '10');
 
             // 测试负数
+            let b2 = new Binary(-1, 32);
+            assert.equal(b2.toBinaryString(), '11111111' + '11111111' + '11111111' + '11111111');
+            assert.equal(b2.toHexString(), 'ffff' + 'ffff');
+            assert.equal(b2.toDecimalString(), '-1');
         });
     });
 });
